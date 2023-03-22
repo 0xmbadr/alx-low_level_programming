31054dca2662496003c319ce61a4892770606217
Muhammad Badr <muhammadbadr30@gmail.com>
2023-03-21 19:26:49 -0700

    add solution @0x02_C/#8,9,10,11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @n1: fist input (integer)
+ *
+ * @n2: second input (integer)
+ *
+ * Desc: give two integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
