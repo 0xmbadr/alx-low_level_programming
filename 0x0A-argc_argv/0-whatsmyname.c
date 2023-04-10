ae054c5f39d625f7e20bbebf4a9f20b6f50a501d
MrCarbene <Muhammad_Fouda@proton.me>
2023-04-11 01:21:57 +0200

    :fire: 0x0A_C #0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name.
+ *
+ * @argc: args count
+ *
+ * @argv: args
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
